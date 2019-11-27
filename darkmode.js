b49c9fb29f9d1a0341785039df4f0a9cfbaf3027
Looksh <rlatmdgus720680@gmail.com>
2019-11-28 07:56:59 +0900

    이것저것

--- darkmode.js
+++ darkmode.js
@@ -1,11 +1,12 @@
-var Body = { //다크모드를 실행시키기 위한 함수
-    setColor:function (color){
-        document.querySelector('body').style.color = color;
-    },
-    setBackgroundColor:function (color){
-        document.querySelector('body').style.backgroundColor = color;
-    }
-}
+var Body = {
+  //다크모드를 실행시키기 위한 함수
+  setColor: function(color) {
+    document.querySelector("body").style.color = color;
+  },
+  setBackgroundColor: function(color) {
+    document.querySelector("body").style.backgroundColor = color;
+  }
+};
 
 // var Card = {
 //     setColorImg:function (){
@@ -13,36 +14,50 @@ var Body = { //다크모드를 실행시키기 위한 함수
 //     }
 // }
 
-var Handler = { //input 변경을 위한 함수
-    setColorHandler:function (color){
-        document.getElementById('dark_white').style.color = color;
-    },
-    setBackgroundColorHandler:function (color){
-        document.getElementById('dark_white').style.backgroundColor = color;
-    }
+var Handler = {
+  //input 변경을 위한 함수
+  setColorHandler: function(color) {
+    document.getElementById("dark_white").style.color = color;
+    document.getElementById("name").style.color = color;
+  },
+  setBackgroundColorHandler: function(color) {
+    document.getElementById("dark_white").style.backgroundColor = color;
+    document.getElementById("name").style.backgroundColor = color;
+  }
+};
+
+function DarkHandler(self) {
+  var target = document.querySelector("body");
+  if (self.value === "dark") {
+    Body.setBackgroundColor("black");
+    Body.setColor("#FF55A1");
+    self.value = "acua";
+
+    //다크모드 변경할 때 배경과 텍스트 색상
+    Handler.setColorHandler("black");
+    Handler.setBackgroundColorHandler("#69d2e7");
+    // Card.style.boxShadow('5px 10px 15px #69d2e7');
+  } else {
+    Body.setBackgroundColor("#69d2e7");
+    Body.setColor("black");
+    self.value = "dark";
+
+    //다크모드 변경할 때 배경과 텍스트 색상
+    Handler.setColorHandler("#69d2e7");
+    Handler.setBackgroundColorHandler("black");
+    // Card.style.boxShadow('0 8px 16px 0 rgba(0,0,0,0.2)');
+  }
 }
 
-function DarkHandler(self){
-    var target = document.querySelector('body');
-    if(self.value === 'dark'){
-        Body.setBackgroundColor('black');
-        Body.setColor('#FF55A1')
-        self.value = 'acua';
-
-        //다크모드 변경할 때 배경과 텍스트 색상
-        Handler.setColorHandler('black');
-        Handler.setBackgroundColorHandler('#69d2e7');
-        // Card.style.boxShadow('5px 10px 15px #69d2e7');
+function inputName() {
+  const nameValue = document.getElementById("name").value;
+  if (event.keyCode == 13) {
+    if (nameValue == "김승현") {
+      alert("음..비슷함");
     } else {
-        Body.setBackgroundColor('#69d2e7');
-        Body.setColor('black');
-        self.value = 'dark';
-
-        //다크모드 변경할 때 배경과 텍스트 색상
-        Handler.setColorHandler('#69d2e7');
-        Handler.setBackgroundColorHandler('black');
-        // Card.style.boxShadow('0 8px 16px 0 rgba(0,0,0,0.2)');
+      alert("틀렸음");
     }
+  }
 }
 
 // function CardHandler(self){
@@ -53,4 +68,3 @@ function DarkHandler(self){
 //         Card.style["boxShadow"]('0 8px 16px 0 rgba(0,0,0,0.2)');
 //     }
 // }
-
